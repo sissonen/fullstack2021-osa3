f0af1dd4601fd0c769f821b184b4e165ffad4cae
Kalle Sissonen <kalle.sissonen@vincit.fi>
2021-11-23 14:53:14 +0200

    Teht 3.11: puhelinluettelo full stack

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 
+app.use(express.static('puhelinluettelo/build'))
 app.use(express.json())
 app.use(cors())
 app.use(morgan((tokens, req, res) => {
---
+++ puhelinluettelo/build/precache-manifest.b4f8c804ba36ffec80d411d0de3a3018.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a5c01a234243fe7dd1eb1db1d21283f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d45b4efd17cf2f4f6dde",
+    "url": "/static/js/2.035dddb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.035dddb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59f5f730ac5f5ccbe4eb",
+    "url": "/static/js/main.c06370b5.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
